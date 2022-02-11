8d6ad932afd1f15bc40c19f7969b140e4cd18c69
Mayanand <72348797+yMayanand@users.noreply.github.com>
2022-02-11 17:32:04 +0530

    Update train.py

--- train.py
+++ train.py
@@ -1,8 +1,10 @@
-import fastbook
-from torchvision import models, transforms
+import fastbook # only for google colab else comment out this
 from fastai.vision.all import *
+from torch import optim, nn
+from torchvision import models, transforms
 from data import Dataset, get_dl
 from model import get_model
+from functools import partial
 
 imagenet_stats = ([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
 
@@ -22,5 +24,11 @@ val_tfms = transforms.Compose([
 
 train_dl = get_dl('train', transform=train_tfms, shuffle=True)
 val_dl = get_dl('val', transform=val_tfms)
+
+dls = DataLoaders(train_dl, val_dl)
+
+opt_func = partial(OptimWrapper, opt=optim.Adam)
+
+learn = Learner(dls, model, opt_func=opt_func, loss_func=nn.BCEWithLogitsLoss(), metrics=accuracy_multi)
 
 
